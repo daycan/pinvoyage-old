8c6a39926193245b3a448517d3b94b744a3dae87
David Aycan <david.aycan@gmail.com>
2013-11-24 12:15:33 -0800

    Removed obsolete folders

--- ZZ. Archive/welcome/login.php
+++
@@ -1,6 +0,0 @@
-<?php // login.php
-$db_hostname = 'mysql.pinvoyage.com';
-$db_database = 'pinvoyage';
-$db_username = 'daycan';
-$db_password = 'ghettopass';
-?>
--- login.php
+++
@@ -1,6 +0,0 @@
-<?php // login.php
-$db_hostname = 'localhost:11189';
-$db_database = 'pinvoyage';
-$db_username = 'root';
-$db_password = 'root';
-?>
